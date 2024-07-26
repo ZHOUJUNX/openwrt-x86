f17a7280660277493579d963b9fb32d50450986a
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2024-07-26 18:40:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# Modify DISTRIB_REVISION #
+sed -i 's/R24.7.7/20240726/g' package/lean/default-settings/files/zzz-default-settings
+echo '### Modify DISTRIB_REVISION ###'
+
 # Modify default IP Gate #
 sed -i 's/192.168.1.1/172.16.253.254/g' package/base-files/files/bin/config_generate
 echo '### Updates default IP gate ###'
