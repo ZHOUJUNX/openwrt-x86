1f46be48c8fbcd6b02c399067838e4145b042b62
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2024-07-29 13:11:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify DISTRIB_REVISION #
-sed -i 's/R24.7.7/20240726/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/R24.7.7/20240729/g' package/lean/default-settings/files/zzz-default-settings
 echo '### Modify DISTRIB_REVISION ###'
 
 # Modify default IP Gate #
