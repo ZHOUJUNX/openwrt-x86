efbf36f82f49843ff579a08d795023f060df132c
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2024-12-01 14:18:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,17 +12,21 @@
 
 # Modify default IP Gate #
 sed -i 's/192.168.1.1/172.16.253.254/g' package/base-files/luci2/bin/config_generate
-echo '### Updates default IP gate ###'
+echo '# Updates default IP gate #'
 
 # argon theme plug-in components #
 rm -rf package/feeds/luci/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
-echo '### luci-theme-argon ###'
+echo '# luci-theme-argon #'
 
-#echo '### Fixed Theme Argon /*# sourceMappingURL=cascade.css.map */ error ###'
+# Fixed Theme Argon # ###sourceMappingURL=cascade.css.map */ error ###'
 sed -i 's|/\*# sourceMappingURL=cascade.css.map \*/||g' package/feeds/luci/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-echo '### Fixed Theme Argon ###'
+echo '# Fixed Theme Argon #'
+
+# Argon Theme Config #
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+echo '# Argon Theme Config #'
 
 # shutdown plug-in components #
 git clone https://github.com/ZHOUJUNX/luci-app-poweroff.git package/lean/luci-app-poweroff
-echo '### Shutdown Router ###'
+echo '# Shutdown Router #'
