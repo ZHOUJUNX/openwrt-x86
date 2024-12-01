f864c8324a38de7f371260d896903fbb9d0de4b1
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2024-12-01 14:16:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,26 +23,6 @@ echo '### luci-theme-argon ###'
 sed -i 's|/\*# sourceMappingURL=cascade.css.map \*/||g' package/feeds/luci/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 echo '### Fixed Theme Argon ###'
 
-# argon config plug-in components #
-# git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-# echo '### luci-app-argon-config ###'
-
-# adguardhome plug-in components #
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
-# echo '### luci-app-adguardhome ###'
-
-# smartdns plug-in components #
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
-# echo '### luci-app-smartdns ###'
-
 # shutdown plug-in components #
 git clone https://github.com/ZHOUJUNX/luci-app-poweroff.git package/lean/luci-app-poweroff
 echo '### Shutdown Router ###'
-
-# openappfilter plug-in components #
-# git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
-# echo '### luci-app-oaf ###'
-
-# lucky plug-in components #
-# git clone  https://github.com/gdy666/luci-app-lucky.git package/lean
-# echo '### luci-app-lucky ###'
